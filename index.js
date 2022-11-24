cb1a2d6f3b8a8d2e8568ed2f9c790d398c57fcc3
Tharusha Dilshan <107659630+dilshantharu@users.noreply.github.com>
2022-11-24 18:03:02 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>light</title>
+    <style type = "text/css">
+            #shapes{
+        
+        background-color: green;
+        display: none;
+        position: relative;
+      }
+      
+    </style>
+  </head>
+  
+  <body>
+    <hr>
+    <h1><center>Java script Small Game</center></h1>
+    <hr>
+    
+      <h1><span id="new">You Get : </span></h1>
+    
+    <div id="shapes"></div>
+
+
+    <script src="script.js">  </script>
+    
+    
+  </body>
+  
+</html>
